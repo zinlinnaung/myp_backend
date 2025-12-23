50dae3ec57ffda9e0d0843de6bb3afe3d5ff5bf7
zinlinnaung.bit <zinlinnaung.bit@gmail.com>
2025-12-23 15:18:24 +0630

    updated

--- src/Course/course.service.ts
+++ src/Course/course.service.ts
@@ -18,7 +18,7 @@ export class CourseService {
     const course = await this.prisma.course.course.findUnique({
       where: { id },
       include: {
-        course_section: {
+        CourseSection: {
           include: { activities: true },
         },
       },
