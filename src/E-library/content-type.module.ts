984840a7ac1f953904483bac03eec052ba656391
zinlinnaung.bit <zinlinnaung.bit@gmail.com>
2025-12-29 13:15:45 +0630

    updated

--- src/E-library/content-type.module.ts
+++ src/E-library/content-type.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ContentTypeService } from './content-type.service';
 import { ContentTypeController } from './content-type.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [ContentTypeController],
-  providers: [ContentTypeService],
+  providers: [ContentTypeService, PrismaService],
 })
 export class ContentTypeModule {}
